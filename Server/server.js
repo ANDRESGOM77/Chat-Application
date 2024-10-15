2e45ff79094962f10ee3a7537529567e13e002ca
ANDRESGOM77 <141623667+ANDRESGOM77@users.noreply.github.com>
2024-10-15 20:40:53 +0200

    adding routes

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,16 @@
-const express = require("express")
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.js"
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!!");
+});
+
+app.use("/api/auth", authRoutes)
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
