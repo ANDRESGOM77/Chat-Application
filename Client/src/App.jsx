aaca75092fe81b7a668fa905675059d712f224ad
ANDRESGOM77 <141623667+ANDRESGOM77@users.noreply.github.com>
2024-10-17 19:49:58 +0200

    adding sidebar UI

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,11 @@
 // import Login from "./pages/login/Login";
-import SignUp from "./pages/signUp/signUp";
-// import Home from "./pages/home/Home";
+// import SignUp from "./pages/signUp/signUp";
+import Home from "./pages/homePage/HomePage";
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <SignUp />
+      {/* <SignUp /> */}
+      <Home />
     </div>
   );
 }
--- Client/src/pages/homePage/HomePage.jsx
+++ Client/src/pages/homePage/HomePage.jsx
@@ -1,6 +1,10 @@
+import Sidebar from "../Sidebar/Sidebar"
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div  className="flex h-screen sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
+      <Sidebar />
+      {/* <MessageContainer/> */}
+    </div>
   )
 }
 
