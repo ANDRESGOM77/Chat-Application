6ecc3462c9b55710430b593640ef0ca3100b4f30
ANDRESGOM77 <141623667+ANDRESGOM77@users.noreply.github.com>
2024-10-17 12:54:59 +0200

    adding getMessages

--- Server/controllers/message.js
+++ Server/controllers/message.js
@@ -28,8 +28,27 @@ export const sendMessage = async (req, res) => {
       conversations.messages.push(newMessage._id);
     }
 
+    await Promise.all([conversations.save(), message.save()]);
     res.status(201).json(newMessage);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const getMessage = async (req,res) => {
+    try {
+        const {id:userToChadId} = req.params
+        const senderId = req.user._id
+
+        const conversation = await Conversation.findOne({
+            participants:{$all:[senderId,userToChadId]}
+        }).populate("messages")
+        if(!conversation){
+            res.status(200).json([])
+        }
+        const messages= conversation.messages
+        res.status(200).json(messages)
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
