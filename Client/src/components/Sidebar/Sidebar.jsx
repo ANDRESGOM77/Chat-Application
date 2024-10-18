6c73b6c29df423ee5a7920cc233010cbac2e27f6
ANDRESGOM77 <141623667+ANDRESGOM77@users.noreply.github.com>
2024-10-18 21:31:18 +0200

    adding hooks to get the conversations

--- Client/src/components/Sidebar/Sidebar.jsx
+++ Client/src/components/Sidebar/Sidebar.jsx
@@ -3,12 +3,15 @@ import SearchInput from "./SearchInput"
 import LogoutButton from "./LogoutButton"
 
 
+
 const Sidebar = () => {
+   
+
   return (
     <div className=" border-r border-slate-500 p-4 flex flex-col">
         <SearchInput />
         <div className="divider px-3"></div>
-        <Conversations />
+        <Conversations/>
         <LogoutButton />
     </div>
   )
