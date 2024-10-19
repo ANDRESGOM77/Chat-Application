dafbca359bbedf4bc4119c0b5704a282671312df
ANDRESGOM77 <141623667+ANDRESGOM77@users.noreply.github.com>
2024-10-19 22:17:57 +0200

    fixing sing up in client

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,21 +1,29 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import Toaster from "react-hot-toast"
+import Toaster from "react-hot-toast";
 
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signUp/SignUp";
 import Home from "./pages/homePage/HomePage";
 import { useAuthContext } from "./context/AuthContext";
 function App() {
-
-  const {authUser}=useAuthContext();
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={authUser ? <Home /> : <Navigate to={"/login"}/>} />
-        <Route path="/login" element={authUser ? <Navigate to="/"/> : <Login />} />
-        <Route path="/signup" element={ authUser ? <Navigate to="/"/> : <SignUp />} />
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
       </Routes>
-      <Toaster/>
+      <Toaster />
     </div>
   );
 }
