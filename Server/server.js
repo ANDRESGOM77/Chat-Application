fe4333f35814048618747da0cd2ce83aa30d9837
ANDRESGOM77 <141623667+ANDRESGOM77@users.noreply.github.com>
2024-10-19 21:38:03 +0200

    rendering the app, creating paths to render

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import authRoutes from "./routes/auth.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 import connectToMongoDB from "./config/connection.js";
 import { app, server } from "./socket/socket.js";
+
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json());
@@ -17,6 +19,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/Client/dist")));
+
+app.get("*", (res, req) => {
+  res.sendFile(path.join(__dirname, "Client", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
